4e566c2b26273b6a5cf8e0614b71fac74815af84
Mikky <meekyberry6@gmail.com>
2023-05-17 22:32:49 +0100

    feat: Added favicon

---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// import {}
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOgJ8BaLccvDT4mKBWGTbPM3Ac4zp07KU",
+  authDomain: "machala-laundry-service.firebaseapp.com",
+  projectId: "machala-laundry-service",
+  storageBucket: "machala-laundry-service.appspot.com",
+  messagingSenderId: "346236450495",
+  appId: "1:346236450495:web:a8a5f316a42f2aec5bf80a",
+  measurementId: "G-BPR2EJBRBM"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default firebaseConfig;
